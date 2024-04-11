98daca91b90eb885ec8ef6a0f60b279e82cd872e
dinhanhquoc <126925236+dinhanhquoc@users.noreply.github.com>
2024-04-11 10:01:50 +0700

    ScripAddAndDeleteTable
    ScripAddAndDeleteTable

---
+++ CompanyDB_Simple/AddAndDeleteTable.sql
@@ -0,0 +1,23 @@
+﻿
+/*Tạo bảng Employees*/
+--CREATE TABLE Employees (
+--	Employee INT PRIMARY KEY,
+--	FirstName NVARCHAR(50),
+--	LastName NVARCHAR(50),
+--	DepartmentID INT
+--);
+
+
+-- Tạo bảng Departments
+CREATE TABLE Departments(
+	DepartmentID INT IDENTITY(1,1) PRIMARY KEY,
+	DepartmentName NVARCHAR(50)
+);
+
+DROP TABLE Employees
+
+DROP TABLE Departments
+
+
+
+

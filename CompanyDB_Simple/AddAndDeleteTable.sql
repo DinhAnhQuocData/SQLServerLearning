d2209fd4e198bc2b23c1d88776ba035248b5bf04
dinhanhquoc <126925236+dinhanhquoc@users.noreply.github.com>
2024-04-11 10:09:47 +0700

    Update AddAndDeleteTable.sql
    Fix Table

--- CompanyDB_Simple/AddAndDeleteTable.sql
+++ CompanyDB_Simple/AddAndDeleteTable.sql
@@ -1,16 +1,19 @@
 ﻿
 /*Tạo bảng Employees*/
---CREATE TABLE Employees (
---	Employee INT PRIMARY KEY,
---	FirstName NVARCHAR(50),
---	LastName NVARCHAR(50),
---	DepartmentID INT
---);
+
+
+CREATE TABLE Employees (
+	Employee INT IDENTITY(1000,1) PRIMARY KEY,
+	FirstName NVARCHAR(50),
+	LastName NVARCHAR(50),
+	DepartmentID INT,
+	FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
+);
 
 
 -- Tạo bảng Departments
 CREATE TABLE Departments(
-	DepartmentID INT IDENTITY(1,1) PRIMARY KEY,
+	DepartmentID INT IDENTITY(1000,1) PRIMARY KEY,
 	DepartmentName NVARCHAR(50)
 );
 

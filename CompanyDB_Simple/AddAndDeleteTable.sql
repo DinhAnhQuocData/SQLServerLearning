3cb5acbc8ab9ce33ae0472c94c76d8617b221f4a
dinhanhquoc <126925236+dinhanhquoc@users.noreply.github.com>
2024-04-11 10:25:56 +0700

    Update AddAndDeleteTable.sql

--- CompanyDB_Simple/AddAndDeleteTable.sql
+++ CompanyDB_Simple/AddAndDeleteTable.sql
@@ -1,25 +1,28 @@
 ﻿
-/*Tạo bảng Employees*/
+--/*Tạo bảng Employees*/
 
 
-CREATE TABLE Employees (
-	Employee INT IDENTITY(1000,1) PRIMARY KEY,
-	FirstName NVARCHAR(50),
-	LastName NVARCHAR(50),
-	DepartmentID INT,
-	FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
-);
+--CREATE TABLE Employees (
+--	Employee INT IDENTITY(1000,1) PRIMARY KEY,
+--	FirstName NVARCHAR(50),
+--	LastName NVARCHAR(50),
+--	DepartmentID INT,
+--	FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
+--);
 
+---- Tạo bảng Departments
+----CREATE TABLE Departments(
+----	DepartmentID INT IDENTITY(1000,1) PRIMARY KEY,
+----	DepartmentName NVARCHAR(50)
+----);
 
--- Tạo bảng Departments
-CREATE TABLE Departments(
-	DepartmentID INT IDENTITY(1000,1) PRIMARY KEY,
-	DepartmentName NVARCHAR(50)
-);
+-- Xóa bảng Employees , Departments
+--DROP TABLE Employees
 
-DROP TABLE Employees
+--DROP TABLE Departments
 
-DROP TABLE Departments
+-- Xóa tất cả bảng ghi có trong bảng Departments
+--DELETE FROM Departments;
 
 
 
